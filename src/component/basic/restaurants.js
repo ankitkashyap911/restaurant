0477233666cb8d977e676f2deb9ec352782cc253
ankitkashyap911 <ankitkashyap911@gmail.com>
2022-07-28 18:27:26 +0530

    Second commit

--- src/component/basic/restaurants.js
+++ src/component/basic/restaurants.js
@@ -7,6 +7,7 @@ import Navbar from './Navbar';
 const Uniquelist = [...new Set(Menu.map((curelem) => {
   return curelem.category
 })), "All"]
+//console.log(Uniquelist)
 console.log(Uniquelist)
 //unique list chahiye kyuki category lunch break dinner repeat ho rhe h to uske liye
 //ab navbar alg se file bnane k bad jo humne uniquelist variable m value store krk rkhi h usko ek state m store krk loop chla denge map se  jese card-container m kiya tha 
